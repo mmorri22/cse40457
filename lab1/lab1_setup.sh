4a2ca0c80771831316fa173eb9f10a792a0d803a
Matthew Morrison <mmorri22@nd.edu>
2025-02-25 16:46:36 -0500

    Lab 1 Setup Script

---
+++ lab1/lab1_setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/cse40457/refs/heads/main/lab1/dcl.h
+wget https://raw.githubusercontent.com/mmorri22/cse40457/refs/heads/main/lab1/host.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse40457/refs/heads/main/lab1/makefile
+wget https://raw.githubusercontent.com/mmorri22/cse40457/refs/heads/main/lab1/script.tcl
+wget https://raw.githubusercontent.com/mmorri22/cse40457/refs/heads/main/lab1/top.cpp
+rm lab1_setup.sh
